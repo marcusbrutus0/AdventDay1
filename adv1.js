3be57ac746dc0450a367a94bf5a54e6b3ef6672d
Hardik Sharma <92125252+marcusbrutus0@users.noreply.github.com>
2024-01-17 23:10:43 +0530

    Update adv1.js

--- adv1.js
+++ adv1.js
@@ -1,17 +1,83 @@
+
 const fs = require('node:fs');
 
-function partOne(file){
-    const lines = fs.readFileSync(file , 'utf8').trim().split("\n");
-    
-    const values = lines.map((line) => {
-        
-        let first = line.split('').find((v) => !Number.isNaN(Number(v)));
-        let last  = line.split('').reverse().find((v) => !Number.isNaN(Number(v)));
-        return Number(first + last);
-        
-    });
-    
-    return values.reduce((s,v) => s + v);
-}
 
-partOne('sample.txt');
+const firstnumberWordsRegExp = new RegExp(
+    ['one' , 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    .join('|')
+);
+
+const lastnumberWordsRegExp = new RegExp(
+    ['one' , 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    .join('|').split('').reverse().join('')
+);
+
+
+
+const wordMap = {
+    'one': '1'  ,
+     'two': '2' ,
+      'three': '3' ,
+       'four': '4',
+        'five': '5' ,
+         'six': '6',
+          'seven': '7' ,
+           'eight': '8' ,
+            'nine': '9',
+};
+
+
+
+function partTwo(file){ 
+    const lines = fs.readFileSync(file, "utf-8").trim().split("\r\n")
+
+    const values = lines.map((line) => {
+         let firstNumberIndex = line
+         .split('')
+         .findIndex((v) => !Number.isNaN(Number(v)));
+         let firstWordMatch = line
+         .match(firstnumberWordsRegExp);
+         let firstWordNumberIndex = firstWordMatch?.index;
+
+         let firstNumber = firstNumberIndex != -1 
+         ? firstWordMatch 
+          ? firstNumberIndex < firstWordNumberIndex
+            ? line[firstNumberIndex] 
+            : wordMap[firstWordMatch[0]]
+          : line[firstNumberIndex]
+          : wordMap[firstWordMatch[0]];
+         
+
+         let lastNumberIndex = line
+         .split('')
+         .findLastIndex((v) => !Number.isNaN(Number(v)));
+
+         let lastWordMatch = line
+         .split('')
+         .reverse()
+         .join('')
+         .match(lastnumberWordsRegExp);
+         let lastWordNumberIndex = lastWordMatch? line.length - 1 - lastWordMatch.index : null;
+         
+         let lastNumber = lastNumberIndex != -1 
+         ? lastWordMatch 
+          ? lastNumberIndex < lastWordNumberIndex
+           ? wordMap[lastWordMatch[0].split('').reverse().join('')]
+           : line[lastNumberIndex]
+         : line[lastNumberIndex]
+         : wordMap[lastWordMatch[0].split('').reverse().join('')];
+         
+         
+
+        
+         console.log(firstNumber , lastNumber);
+
+         return Number(firstNumber + lastNumber);
+
+    })  
+
+       return values.reduce((s, v) => s + v);
+
+}
+console.log(partTwo('./input.txt'));
+  
